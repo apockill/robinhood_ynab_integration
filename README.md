0e0db24127123dc04f92b5b0e2b85784c626e5a8
alex <apocthiel@gmail.com>
2020-01-26 17:30:01 -0800

    Initial open sourcing

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# robinhood_ynab_integration
-Sync stocks and cash information to your YNAB!
+# Robinhood ➡️ Ynab
+Import stocks and cash management information to your You Need A Budget (YNAB) account!
+
+If you're an avid user of YNAB you probably already realized that there is currently no way to automatically import Robinhood information. This script + Google Cloud Functions is the way that I've gotten around this. 
+
+# Quick 'how it works'
+Basically, you create two "Unlinked" accounts on your YNAB. 
+
+- Account 1 (Checking): A "Checking" type account. Call it "Checking @ Robinhood" or whatever you like to name your accounts.
+- Account 2 (Stocks): A "Tracking" type account. Call it "Stocks @ Robinhood" or whatever you like to name your equity accounts.
+
+The "Checking" account will reflect purchases, sales, dividends, and transfers in and out of your robinhood. 
+
+The "Stocks" account will be automatically updated to reflect the current value of your Robinhood account. 
---
+++ sync_rh_to_ynab.py
@@ -0,0 +1,205 @@
+import datetime
+import logging
+import os
+
+import dateutil
+import ynab_client
+from Robinhood import Robinhood
+
+
+def get_robinhood_info(rh: Robinhood):
+    """
+    :param rh: The logged-in robinhood object
+    :return: $ in stocks, $ not allocated
+    """
+    equity = rh.equity()
+    positions = rh.positions()["results"]
+    stock_assets = 0
+    for position in positions:
+        num_stocks = float(position["quantity"])
+        symbol = rh.get_url(position["instrument"])["symbol"]
+
+        quote = rh.get_quote(symbol)
+        price_str = quote["last_extended_hours_trade_price"]
+        if price_str is None:
+            price_str = quote["last_trade_price"]
+
+        stock_assets += float(price_str) * num_stocks
+    return stock_assets, equity - stock_assets
+
+
+def make_transaction(amount, budget_id, account_id, memo, date=None,
+                     approved=False):
+    amount = int(amount * 1000)
+    date = date or datetime.datetime.now().isoformat()
+    transaction = ynab_client.SaveTransaction(
+        account_id=account_id,
+        amount=amount,
+        date=str(date),
+        cleared="cleared",
+        approved=approved
+    ).to_dict()
+    transaction["memo"] = f"[AUTOMATED] {memo}"
+    transaction["payee_name"] = "RobinhoodAPI@Alex"
+    transactions_api = ynab_client.TransactionsApi()
+
+    transactions_api.create_transaction(
+        budget_id=budget_id,
+        transaction=ynab_client.SaveTransactionWrapper(transaction))
+
+
+def get_ynab_accounts(ynab_api_key, holding_acc_name, assets_acc_name):
+    # Log in to YNAB
+    config = ynab_client.Configuration()
+    config.api_key_prefix["Authorization"] = "Bearer"
+    config.api_key["Authorization"] = ynab_api_key
+    client = ynab_client.ApiClient(configuration=config)
+
+    # Get Budgets
+    budgets_api = ynab_client.BudgetsApi()
+    budget_id = budgets_api.get_budgets().data.budgets[0].id
+    budget = budgets_api.get_budget_by_id(budget_id)
+
+    # Get accounts
+    accounts_api = ynab_client.AccountsApi()
+    accounts = accounts_api.get_accounts(budget_id=budget_id).data.accounts
+
+    holding_acc = next(a for a in accounts if a.name == holding_acc_name)
+    assets_acc = next(a for a in accounts if a.name == assets_acc_name)
+
+    return budget_id, holding_acc, assets_acc
+
+
+def parse_iso_date(date_str) -> datetime.datetime:
+    return dateutil.parser.parse(date_str).replace(tzinfo=None)
+
+
+def is_old_date(date, last_update_date):
+    if not isinstance(date, datetime.datetime):
+        date = parse_iso_date(date)
+    return (date - last_update_date).total_seconds() < 0
+
+
+def sync_robinhood_to_ynab(
+        robinhood_assets_acc: str,
+        robinhood_holding_acc: str,
+        ynab_api_key: str,
+        robinhood_username: str,
+        robinhood_pass: str,
+        robinhood_qr_code: str):
+    last_rh_update_date = datetime.datetime.now() - datetime.timedelta(hours=8)
+
+    # Log in to Robinhood
+    trader = Robinhood()
+    trader.login(
+        username=robinhood_username,
+        password=robinhood_pass,
+        qr_code=robinhood_qr_code), "Unable to log in to Robinhood!"
+
+    #### Update Assets Account ####
+    real_asset_dollars, real_holding_dollars = get_robinhood_info(trader)
+
+    budget_id, holding_acc, assets_acc = get_ynab_accounts(
+        ynab_api_key=ynab_api_key,
+        holding_acc_name=robinhood_holding_acc,
+        assets_acc_name=robinhood_assets_acc)
+
+    # Make adjustments to the assets account
+    asset_dollars = assets_acc.balance / 1000
+    asset_adjustment = round(real_asset_dollars - asset_dollars, 3)
+    if asset_adjustment != 0:
+        logging.info("Making adjustment", asset_adjustment)
+        make_transaction(
+            amount=asset_adjustment,
+            budget_id=budget_id,
+            account_id=assets_acc.id,
+            memo="Daily Account Adjustment",
+            approved=True)
+    else:
+        logging.info("No Asset Adjustment Needed", asset_dollars,
+                     asset_adjustment)
+
+    #### Update Holdings Account ####
+    # Update incoming and outgoing transfers
+    transfers = trader.get_transfers()["results"]
+    for transfer in transfers:
+        # Only process transactions since the last time the script ran
+        if is_old_date(transfer["created_at"], last_rh_update_date):
+            continue
+
+        # Add transaction to robinhood
+        sign = 1 if transfer["direction"] == "deposit" else -1
+        make_transaction(
+            amount=float(transfer["amount"]) * sign,
+            budget_id=budget_id,
+            date=transfer["created_at"],
+            account_id=holding_acc.id,
+            memo="Robinhood Transfer")
+        logging.info("Adding Transaction for Transfer",
+                     transfer["amount"] * sign)
+
+    # Update internal stock purchases and sales
+    order_history = trader.order_history()["results"]
+    for order in order_history:
+        filled = order["state"] == "filled"
+        if not filled or len(order["executions"]) == 0:
+            logging.info("Ignoring order because it is not marked 'filled'."
+                         f" Order {order}")
+            continue
+
+        last_transaction_time = parse_iso_date(order["last_transaction_at"])
+        if is_old_date(last_transaction_time, last_rh_update_date):
+            continue
+
+        symbol = trader.get_url(order["instrument"])["symbol"]
+
+        outflow = 0
+        for execution in order["executions"]:
+            price = float(execution["price"])
+            quantity = float(execution["quantity"])
+            outflow += price * quantity
+
+        sign = -1 if order["side"] == "buy" else 1
+        logging.info("Adding transaction for stock order",
+                     symbol, outflow * sign, last_transaction_time)
+        make_transaction(
+            amount=outflow * sign,
+            budget_id=budget_id,
+            account_id=holding_acc.id,
+            date=last_transaction_time,
+            memo=f"Robinhood {symbol} {'Purchased' if sign < 0 else 'Sold'}"
+        )
+
+    # Update dividend payouts
+    dividends = trader.dividends()["results"]
+    for dividend in dividends:
+        payment_date = parse_iso_date(dividend["paid_at"])
+        amount = dividend["amount"]
+
+        # Make sure this is a new transaction
+        if is_old_date(dividend["paid_at"], last_rh_update_date):
+            continue
+        symbol = trader.get_url(dividend["instrument"])["symbol"]
+        logging.info("Adding Dividend Adjustment", payment_date, amount)
+        make_transaction(
+            amount=float(amount),
+            budget_id=budget_id,
+            account_id=holding_acc.id,
+            memo=f"Dividend from {symbol}",
+            date=payment_date)
+
+
+def main(request):
+    logging.info("Request", request)
+    sync_robinhood_to_ynab(
+        # Lot's of secrets
+
+        robinhood_username=os.environ["RH_USERNAME"],
+        robinhood_pass=os.environ["RH_PASS"],
+        robinhood_qr_code=os.environ["RH_QR_CODE"],
+        ynab_api_key=os.environ["YNAB_API_KEY"],
+
+        # General Arguments
+        robinhood_assets_acc=os.environ["RH_ASSETS_ACC"],
+        robinhood_holding_acc=os.environ["RH_CHECKING_ACC"]
+    )
