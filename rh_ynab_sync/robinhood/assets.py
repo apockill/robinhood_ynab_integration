3349d31533e0c17556c7eb655de79fc5df2569e4
Alex Thiel <apocthiel@gmail.com>
2021-11-02 19:35:59 -0600

    Add support for syncing Crypto in the get_total_assets_value function

--- rh_ynab_sync/robinhood/assets.py
+++ rh_ynab_sync/robinhood/assets.py
@@ -1,12 +1,18 @@
 from Robinhood import Robinhood, exceptions
 
 
-def get_total_assets_value(rh: Robinhood):
+def get_total_assets_value(rh: Robinhood) -> float:
     """
     :param rh: The logged-in robinhood object
     :return: $ in stocks, $ not allocated (Cash)
     """
-    equity = rh.equity()
+    stocks_value = _get_stock_assets_value(rh)
+    crypto_value = _get_crypto_assets_value(rh)
+    return crypto_value + stocks_value
+
+
+def _get_stock_assets_value(rh: Robinhood) -> float:
+    """Returns the total value held in stocks"""
     positions = rh.positions()["results"]
     stock_assets = 0
     for position in positions:
@@ -31,4 +37,22 @@ def get_total_assets_value(rh: Robinhood):
             price_str = quote["last_trade_price"]
 
         stock_assets += float(price_str) * num_stocks
-    return stock_assets, equity - stock_assets
+    return stock_assets
+
+
+def _get_crypto_assets_value(rh: Robinhood) -> float:
+    """Returns the total value held in crypto"""
+    # print(rh.crypto_orders())
+    crypto_assets = 0
+    crypto_holdings_data = rh.crypto_holdings()["results"]
+
+    for crypto_holding in crypto_holdings_data:
+        quantity_available = float(crypto_holding["quantity_available"])
+        if quantity_available == 0:
+            continue
+
+        code = crypto_holding["currency"]["code"]
+        market_price_str = rh.crypto_quote(rh.CRYPTO_PAIRS[code])["mark_price"]
+
+        crypto_assets += quantity_available * float(market_price_str)
+    return crypto_assets
